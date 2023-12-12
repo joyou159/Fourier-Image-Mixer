4730961f67737cf89f278d915eac1f8a9496ef10
MohamedMandour10 <mhmdabomandour11@gmail.com>
2023-12-12 08:36:50 +0200

    change layout

--- imageViewPort.py
+++ imageViewPort.py
@@ -2,134 +2,78 @@ import sys
 from PyQt6.QtWidgets import (
     QApplication,
     QWidget,
-    QVBoxLayout,
-    QHBoxLayout,
-    QLabel,
-    QComboBox,
+    QVBoxLayout,  
 )
-from PyQt6.QtGui import QPixmap, QImage
-from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap, QImage,  QPainter
+from PyQt6.QtCore import Qt, QSize
 from PyQt6 import QtWidgets
 
+from PIL import Image, ImageQt, ImageEnhance
 # Placeholder for FT-related functionalities
 import numpy as np
 from scipy.fft import fft2, ifft2, fftshift
 
+
 class ImageViewport(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.image = None
-        self.ft_components = {}  # Store calculated FT components
+        self.images = []  # List to store all opened images
+        self.resized_imgs = []
 
-        self.image_label = QLabel()
-        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.brightness = 0
+        self.contrast = 100       
 
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.image_label)
-        self.setLayout(layout)
+        self.layout = QVBoxLayout(self)
+        self.setLayout(self.layout)
 
 
+    def calculate_min_size(self):
+        if self.resized_imgs:
+            min_width = min(img.width for img in self.images)
+            min_height = min(img.height for img in self.images)
+            return min_width, min_height
+        else:
+            return None, None
 
     def set_image(self, image_path):
         try:
-            image = QImage(image_path)
-            if image.format() == QImage.Format.Format_Grayscale8:
-                self.set_image_from_qimage(image)
-            else:
-                self.set_image_from_qimage(image.convertToFormat(QImage.Format.Format_Grayscale8))
+            image = Image.open(image_path).convert('L')  # Convert to grayscale
+            self.images.append(image)
+            self.update_display()
         except Exception as e:
             print(f"Error opening image: {e}")
 
-
-
-    def set_image_from_qimage(self, image):
-        self.image = image
-
-        # If the widget has been shown, calculate and set the initial display
-        if self.isVisible():
-            self.update_display()
-
-
     def update_display(self):
-            if self.image is not None:
-                pixmap = QPixmap.fromImage(self.image)
-                self.image_label.setPixmap(pixmap.scaled(self.width(), self.height(), Qt.AspectRatioMode.KeepAspectRatio))
+        if self.images:
+            self.repaint()
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        if self.images:
+            painter = QPainter(self)
+
+            # Draw the images onto the widget using the minimum width and height
+            x, y = 0, 0
+            for img in self.images:
+                adjusted_img = self.adjust_brightness_contrast(img)
+                resized_img = adjusted_img.resize((self.width(), self.height()))
+                pixmap = QPixmap.fromImage(ImageQt.ImageQt(resized_img))
+                painter.drawPixmap(x, y, pixmap)
+
+            painter.end()
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self.update_display()
 
 
-    def update_component(self, index):
-        component = self.component_selector.currentText()
-        if component == "Original":
-            self.component_label.setPixmap(None)
-        else:
-            try:
-                ft_image = self.get_ft_component(component)
-                pixmap = QPixmap.fromImage(ft_image)
-                self.component_label.setPixmap(pixmap.scaled(self.width(), self.height(), Qt.AspectRatioMode.KeepAspectRatio))
-            except KeyError:
-                # Calculate and store the FT component
-                self.calculate_ft_components()
-                self.update_component(index)
+    def adjust_brightness_contrast(self, img):
+        # Adjust brightness and contrast
+        enhancer = ImageEnhance.Brightness(img)
+        img = enhancer.enhance((self.brightness + 255) / 255.0)
+        enhancer = ImageEnhance.Contrast(img)
+        img = enhancer.enhance((self.contrast + 127) / 127.0)
+
+        return img
 
 
-    def get_ft_component(self, component):
-        return self.ft_components[component]
-
-
-    # Placeholder for FT calculations
-    def calculate_ft_components(self):
-        if self.image is not None:
-            image_array = np.array(self.image.convertToFormat(QImage.Format.Format_Grayscale8))
-
-            # Calculate 2D Fourier Transform
-            ft_image = fft2(image_array)
-
-            # Shift zero frequency components to the center
-            ft_image_shifted = fftshift(ft_image)
-
-            # Calculate magnitude, phase, real, and imaginary components
-            ft_magnitude = np.abs(ft_image_shifted)
-            ft_phase = np.angle(ft_image_shifted)
-            ft_real = np.real(ft_image_shifted)
-            ft_imaginary = np.imag(ft_image_shifted)
-
-            # Store the calculated components
-            self.ft_components["FT Magnitude"] = QImage(
-                ft_magnitude.data, ft_magnitude.shape[1], ft_magnitude.shape[0], QImage.Format.Format_Grayscale8)
-            
-            self.ft_components["FT Phase"] = QImage(
-                ft_phase.data, ft_phase.shape[1], ft_phase.shape[0], QImage.Format.Format_Grayscale8)
-            
-            self.ft_components["FT Real"] = QImage(
-                ft_real.data, ft_real.shape[1], ft_real.shape[0], QImage.Format.Format_Grayscale8)
-            
-            self.ft_components["FT Imaginary"] = QImage(
-                ft_imaginary.data, ft_imaginary.shape[1], ft_imaginary.shape[0], QImage.Format.Format_Grayscale8)
-
-
-
-class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.viewports = []
-
-        # Create four viewports
-        for _ in range(4):
-            viewport = ImageViewport()
-            self.viewports.append(viewport)
-
-        layout = QHBoxLayout()
-        for viewport in self.viewports:
-            layout.addWidget(viewport)
-        
-        self.setLayout(layout)
-        self.resize(800, 600)
-        self.setWindowTitle("Image Viewer")
-
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
--- main.py
+++ main.py
@@ -28,12 +28,95 @@ class MainWindow(QtWidgets.QMainWindow):
     def init_ui(self):
         # Load the UI Page
         self.ui = uic.loadUi('Mainwindow.ui', self)
-        self.setWindowTitle("FT Mixer")
+        self.setWindowTitle("Image Mixer")
+        self.setWindowIcon(QIcon("icons/mixer.png"))
         self.image_ports = []
         self.load_ui_elements()
 
+        self.showFullScreen()
+        self.ui.keyPressEvent = self.keyPressEvent
+
+    def keyPressEvent(self, event):
+        # Handle key events, for example, pressing ESC to exit full screen
+        if event.key() == Qt.Key.Key_Escape:
+            self.showNormal()  # Show the window in normal size
+        else:
+            super().keyPressEvent(event)
+
+
+
+    def load_ui_elements(self):
+        """
+        Load UI elements.
+
+        This function is responsible for loading the UI elements of the application. It initializes and configures various UI components 
+        such as view ports, combo boxes, sliders, and connects them to the appropriate event handlers.
+
+        """
+        # List of original UI view ports
+        ui_view_ports = [self.ui.original1, self.ui.original2, self.ui.original3, self.ui.original4]
+
+        # List of combo boxes for UI
+        ui_combo_boxes = [self.ui.combo1, self.ui.combo2, self.ui.combo3, self.ui.combo4]
+
+        # List of combo boxes for mixing UI
+        self.ui_mixing_combo_boxes = [self.ui.combo11, self.ui.combo12, self.ui.combo13, self.ui.combo14]
+
+        # Create image viewports and bind browse_image function to the event
+        self.image_ports.extend([
+            self.create_image_viewport(ui_view_ports[i], lambda event, index=i: self.browse_image(event, index))
+            for i in range(4)
+        ])
+
+        # Add items to combo boxes for UI
+        self.add_items_to_combo_boxes(ui_combo_boxes, ["Magnitude", "Phase", "Real", "Imaginary"])
+
+        # Add items to combo boxes for mixing UI
+        self.add_items_to_combo_boxes(self.ui_mixing_combo_boxes, [f'image{i+1}' for i in range(4)])
+
+        # List of brightness sliders
+        self.ui_brightness_sliders = [getattr(self.ui, f"brightnessSlider{i+1}") for i in range(4)]
+
+        # List of contrast sliders
+        self.ui_contrast_sliders = [getattr(self.ui, f"contrastSlider{i+1}") for i in range(4)]
+
+        # Set minimum, maximum, and initial value for brightness and contrast sliders
+        for slider in self.ui_brightness_sliders + self.ui_contrast_sliders:
+            slider.setMinimum(-255)
+            slider.setMaximum(255)
+            slider.setValue(0)
+            slider.valueChanged.connect(self.update_slider)
+
+
+    def browse_image(self, event, index: int) -> None:
+        """
+        Browse for an image file and set it for the ImageViewport at the specified index.
+        
+        Args:
+            event: The event that triggered the image browsing.
+            index: The index of the ImageViewport to set the image for.
+        """
+        file_filter = "Raw Data (*.png *.jpg *.jpeg *.jfif)"
+        image_path, _ = QtWidgets.QFileDialog.getOpenFileName(
+            None, 'Open Signal File', './', filter=file_filter)
+        
+        if image_path and 0 <= index < len(self.image_ports):
+            self.image_ports[index].set_image(image_path)
+            
+
 
     def create_image_viewport(self, parent, mouse_double_click_event_handler):
+        """
+        Creates an image viewport and adds it to the specified parent widget.
+
+        Args:
+            parent: The parent widget to which the image viewport will be added.
+            mouse_double_click_event_handler: The event handler function to be called when a mouse double-click event occurs on the image viewport.
+
+        Returns:
+            The created image viewport.
+
+        """
         image_port = ImageViewport()
         image_layout = QVBoxLayout(parent)
         image_layout.addWidget(image_port)
@@ -41,35 +124,33 @@ class MainWindow(QtWidgets.QMainWindow):
         return image_port
 
 
-    def load_ui_elements(self):
-        ui_view_ports = [self.ui.original1, self.ui.original2, self.ui.original3, self.ui.original4]
-        ui_combo_boxes = [self.ui.combo1, self.ui.combo2, self.ui.combo3, self.ui.combo4]
-        # Create an empty list to store ImageViewport instances
-        
-        # Create ImageViewport instances using the function
-        self.image_ports.extend([
-            self.create_image_viewport(ui_view_ports[0], lambda event: self.browse_image(event, 0)),
-            self.create_image_viewport(ui_view_ports[1], lambda event: self.browse_image(event, 1)),
-            self.create_image_viewport(ui_view_ports[2], lambda event: self.browse_image(event, 2)),
-            self.create_image_viewport(ui_view_ports[3], lambda event: self.browse_image(event, 3))
-        ])
-        
-        for combo_box in ui_combo_boxes:
-            combo_box.addItems(["Magnitude", "Phase", "Real", "Imaginary"])
+    def add_items_to_combo_boxes(self, combo_boxes, items):
+        """
+        Adds the specified items to the given combo boxes.
+
+        Args:
+            combo_boxes (list): A list of combo boxes to which the items will be added.
+            items (list): A list of items to be added to the combo boxes.
+
+        Returns:
+            None
+        """
+        for combo_box in combo_boxes:
+            combo_box.addItems(items)
 
 
-    def browse_image(self, event, index):
-        file_filter = "Raw Data (*.png *.jpg *.jpeg)"
-        image_path, _ = QtWidgets.QFileDialog.getOpenFileName(
-            None, 'Open Signal File', './', filter=file_filter)
-        
-        if image_path:
-            if 0 <= index < len(self.image_ports):
-                # Set the image only for the ImageViewport at the specified index
-                self.image_ports[index].set_image(image_path)
-
-
-
+    def update_slider(self):
+        """
+        Update the brightness and contrast values of each image port based on the slider values.
+        """
+        # Iterate over the brightness and contrast sliders and their corresponding image ports
+        for index, (brightness_slider, contrast_slider) in enumerate(zip(self.ui_brightness_sliders, self.ui_contrast_sliders)):
+            # Update the brightness and contrast values of the image port
+            self.image_ports[index].brightness = brightness_slider.value()
+            self.image_ports[index].contrast = contrast_slider.value()
+            # Update the display of the image port
+            self.image_ports[index].update_display()
+            
 
 def main():
     app = QtWidgets.QApplication([])
